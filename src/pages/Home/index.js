f406437cc2727ab09f8003c2ad66fbdfb9a9514d
Augusto Kestering Longo <augusto.kestering@gmail.com>
2020-01-27 22:32:52 -0300

    Add style to home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,67 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { ProductList } from './styles';
 
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-adidas-solar-drive-boost-19-masculino/72/NQQ-1406-172/NQQ-1406-172_detalhe2.jpg?ims=326x"
+          alt="Tenis"
+        />
+        <strong>Tênis legal</strong>
+        <span>R$ 129,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-adidas-solar-drive-boost-19-masculino/72/NQQ-1406-172/NQQ-1406-172_detalhe2.jpg?ims=326x"
+          alt="Tenis"
+        />
+        <strong>Tênis legal</strong>
+        <span>R$ 129,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-adidas-solar-drive-boost-19-masculino/72/NQQ-1406-172/NQQ-1406-172_detalhe2.jpg?ims=326x"
+          alt="Tenis"
+        />
+        <strong>Tênis legal</strong>
+        <span>R$ 129,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-adidas-solar-drive-boost-19-masculino/72/NQQ-1406-172/NQQ-1406-172_detalhe2.jpg?ims=326x"
+          alt="Tenis"
+        />
+        <strong>Tênis legal</strong>
+        <span>R$ 129,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
