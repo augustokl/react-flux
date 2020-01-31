c14b841b99a087e8d324d101a543dd3e0ae8e0fb
Augusto Kestering Longo <augusto.kestering@gmail.com>
2020-01-30 21:49:39 -0300

    add amount control

--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -1,13 +1,43 @@
-import { call, put, all, takeLatest } from 'redux-saga/effects';
+import { call, put, all, select, takeLatest } from 'redux-saga/effects';
 
+import { toast } from 'react-toastify';
 import api from '../../../services/api';
+import { formatPrice } from '../../../util/format';
 
-import { addToCartSuccess } from './actions';
+import { addToCartSuccess, updateAmount } from './actions';
 
 function* addToCart({ id }) {
+  const productExists = yield select(state =>
+    state.cart.find(p => p.id === id)
+  );
+
+  const stock = yield call(api.get, `/stock/${id}`);
+  const stockAmount = stock.data.amount;
+
+  const currentAmount = productExists ? productExists.amount : 0;
+
+  const amount = currentAmount + 1;
+
+  if (amount > stockAmount) {
+    toast.error('Produto fora do estoque');
+    return;
+  }
+
+  if (productExists) {
+    yield put(updateAmount(productExists.id, amount));
+
+    return;
+  }
+
   const response = yield call(api.get, `/products/${id}`);
 
-  yield put(addToCartSuccess(response.data));
+  const data = {
+    ...response.data,
+    amount: 1,
+    priceFormatted: formatPrice(response.data.price),
+  };
+
+  yield put(addToCartSuccess(data));
 }
 
 export default all([takeLatest('@cart/ADD_REQUEST', addToCart)]);
